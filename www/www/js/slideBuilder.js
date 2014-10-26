4f54b971d0839840a7f358166a5cc55d6d22c9eb
John Resig <jeresig@gmail.com>
2014-10-26 11:00:49 -0400

    Don't enable Google Analytics on the mobile app.

--- www/www/js/slideBuilder.js
+++ www/www/js/slideBuilder.js
@@ -446,17 +446,6 @@ var Slides = {
     }
 };
 
-
-// Google analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-55986641-1', 'auto');
-  ga('send', 'pageview');
-
-
 // Add app class if we're in a Cordova app
 window.isApp = !!window.cordova;
 if (window.isApp) {
@@ -466,4 +455,16 @@ if (window.isApp) {
 // on a mobile device
 } else if (/android|ios|ipad|iphone|ipod/i.test(navigator.userAgent)) {
     window.location = "mobile.html";
+}
+
+
+if (!window.isApp) {
+    // Google analytics
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-55986641-1', 'auto');
+    ga('send', 'pageview');
 }
